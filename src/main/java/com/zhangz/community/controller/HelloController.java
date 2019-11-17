9c365c4e5b58e9c254fba62f44762b3884bacb8f
zhangzhenoooo <1804919062@qq.com>
2019-11-17 16:28:58 +0545

    test

---
+++ src/main/java/com/zhangz/community/controller/HelloController.java
@@ -0,0 +1,20 @@
+package com.zhangz.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * @author zhangz
+ * @version 1.0
+ * @date 2019/11/17 16:01
+ */
+@Controller
+public class HelloController {
+    @GetMapping("/hello")
+    public  String hello(@RequestParam(name = "name",required=false, defaultValue="") String name, Model model){
+        model.addAttribute("name",name);
+        return "hello";
+    }
+}
